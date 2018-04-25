ac3bdd79a943e8de82f0a0f383e3d31c6a0c321c
Bartosz Pluta <barpluta@o2.pl>
2018-04-25 13:14:51 +0200

    Modify script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 var buttonsItemsClass = document.getElementsByClassName('buttons');
 
-console.log(buttonsItemsClass);
-
 var lengthOfButtons = (buttonsItemsClass.length);
 console.log (lengthOfButtons);
 
 for ( i = 0 ; i < lengthOfButtons ; i++) {
 
-		var x = document.getElementsByClassName('buttons').innerText;
-		document.write (x);
+		var x = buttonsItemsClass[i].innerText;
+		alert(x);
 	}
 
